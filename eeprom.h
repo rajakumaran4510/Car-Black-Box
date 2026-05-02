552276815f05e359c36126b8a0ff8fb8a7a588e6
Rajakumaran R <rajakumaran4510@gmail.com>
2026-05-02 11:51:48 +0530

    Add eeprom.h for internal EEPROM read/write functions

---
+++ eeprom.h
@@ -0,0 +1,7 @@
+#ifndef INTERNAL_EEPROM_H
+#define INTERNAL_EEPROM_H
+
+void write_internal_eeprom(unsigned char address, unsigned char data);
+unsigned char read_internal_eeprom(unsigned char address);
+
+#endif
